2b0773d23c16a7ba87f0ba59a9371a6774dd0e24
Estauti <estautib@gmail.com>
2020-09-24 23:00:50 -0300

    Generate task scaffold

---
+++ db/migrate/20200925013733_create_tasks.rb
@@ -0,0 +1,13 @@
+class CreateTasks < ActiveRecord::Migration[5.2]
+  def change
+    create_table :tasks do |t|
+      t.string :title, null: false
+      t.string :description, null: false, default: ""
+      t.datetime :due_date, null: false
+      t.integer :priority, null: false
+      t.integer :status, null: false
+
+      t.timestamps
+    end
+  end
+end
