ddcaeb8b0450481561d5dffd5bb453e7877fc2b8
Estauti <estautib@gmail.com>
2020-09-25 00:49:03 -0300

    Add task title and description model tests

--- spec/models/task_spec.rb
+++ spec/models/task_spec.rb
@@ -1,5 +1,38 @@
 require 'rails_helper'
 
 RSpec.describe Task, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'Create' do
+    describe 'is valid' do
+      it 'with all required attributes given' do
+        task = build(:task)
+        expect(task).to be_valid
+      end
+    end
+
+    describe 'is invalid' do
+      context 'when title' do
+        it 'is null' do
+          task = build(:task, title: nil)
+          expect(task).to be_invalid
+        end
+
+        it 'is empty' do
+          task = build(:task, title: "")
+          expect(task).to be_invalid
+        end
+      end
+
+      context 'when description' do
+        it 'is null' do
+          task = build(:task, description: nil)
+          expect(task).to be_invalid
+        end
+
+        it 'is empty' do
+          task = build(:task, description: "")
+          expect(task).to be_invalid
+        end
+      end
+    end
+  end
 end
