9a7383c4331b20873e6befa34c28f4b5507a6d4a
Estauti <estautib@gmail.com>
2020-09-27 15:02:40 -0300

    Show responsible on task views

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -73,6 +73,6 @@ class TasksController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def task_params
-      params.require(:task).permit(:title, :description, :due_date, :priority, :status)
+      params.require(:task).permit(:title, :description, :due_date, :priority, :status, :responsible_id)
     end
 end
